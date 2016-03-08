6466c8653e6a35762de29fd15beb680523a89ca1
Sam Bartlett <sambartlett@Sams-MacBook-Pro.local>
2016-03-07 22:09:04 -0600

    Updated client. brought to working point.

--- client/client.cpp
+++ client/client.cpp
@@ -10,6 +10,7 @@
  // Other incldues
  #include <iostream>
  #include <string.h>
+ #include <fstream>
  // For now we will stay with std namespace
  using namespace std;
 
@@ -21,7 +22,7 @@
 
 	int s; // store the socket id
 	string serverIP = argv[1]; // The server ip
-	string fileName = argv[2];
+	string fileName = argv[2]; // The file name
 	// Declare storages for our server and client addresses
 	struct sockaddr_in server;
 	struct sockaddr_in client; 
@@ -52,12 +53,13 @@
  	}
 
  	// Send a get request
- 	string message = "GET " + fileName;
-	if(!sendGet(s, message, (struct sockaddr *)&server, sizeof(server))) {
+	if(!sendGet(s, fileName, (struct sockaddr *)&server, sizeof(server))) {
 		cout << "Sending GET request failed. Quiting." << endl;
 	}
 
 	// Get the file
+	int serverSizeInt = sizeof(server);
+	getFile(fileName, s, (struct sockaddr *)&server, (socklen_t*)&serverSizeInt);
 
 	return closeSocket(s);
 }
@@ -73,8 +75,41 @@ int initSocket() {
 }
 
 // Function to send get request
-bool sendGet(int s, string message, struct sockaddr * server, int serverSize) {
-	return (sendto(s, message.c_str(), BUFFSIZE, 0, server, serverSize)) >= 0; // 0 is flags
+bool sendGet(int s, string fileName, struct sockaddr * server, int serverSize) {
+ 	string message = "GET " + fileName;
+	return (sendto(s, message.c_str(), PACKETSIZE, 0, server, serverSize)) >= 0; // 0 is flags
+}
+
+bool sendACK(int s, struct sockaddr * server, int serverSize) {
+	string message = "ACK";
+	return (sendto(s, message.c_str(), PACKETSIZE, 0, server, serverSize)) >= 0; // 0 is flags
+}
+
+bool sendNAK(int s, struct sockaddr * server, int serverSize) {
+	string message = "NAK";
+	return (sendto(s, message.c_str(), PACKETSIZE, 0, server, serverSize)) >= 0; // 0 is flags
+}
+
+bool getFile(string fileName, int s, struct sockaddr * server, socklen_t * serverSize) {
+	cout << "Waiting for file from the server." << endl;
+	ofstream output;
+	fileName += ".txt";
+	output.open(fileName.c_str());
+	output << "Preping to receive" << endl;
+	for(;;) {
+		byte packet[PACKETSIZE];
+		recvfrom(s, packet, PACKETSIZE, 0, server, serverSize); // 0 is flags
+		cout << "Receiving packet!" << endl;
+		if(packet[0] == '\0') break; // If the content is a null character, it is the end of the file
+
+		// VALIDATE PACKET
+
+		// OUTPUT CONTENT TO FILE
+		output << "DATA GOES IN HERE" << endl;
+	}
+	output.close();
+	cout << "Received final packet" << endl;
+	return true;
 }
 
 // Function to close the socket
--- client/client.h
+++ client/client.h
@@ -12,28 +12,35 @@
  #define NUM_ARGS 3 // 2 args - argv[0] is name, argv[1] is the target server, argv[2] is the desired file name
  #define CLIENT_PORT 10070 // port number assigned to group
  #define SERVER_PORT 10073 // port number assigned to group
- #define BUFFSIZE 512 // Set the buffer size. Packet is 256 and must be able to hold the header, so 512 will be safe for sure.
+ #define PACKETSIZE 256 // Set the buffer size. Packets are 256 so must be 256
+ #define BUFFSIZE 240 // Header will be 16 Bytes
 
  // Header of packet - contains sequence # (0 or 1) and checksum (sum of bytes)
  struct header {
- 	int sequence, checksum;
+ 	bool sequence;
+ 	int checksum;
  };
 
  // Packet- contains header and data
  struct packet {
  	header h;
- 	byte data[256];
+ 	byte data[BUFFSIZE];
  };
 
  int initSocket();
 
- bool sendGet(int s, std::string message, struct sockaddr * server, int serverSize);
+// Send message section
+ bool sendGet(int s, std::string fileName, struct sockaddr * server, int serverSize);
+ bool sendACK(int s, struct sockaddr * server, int serverSize);
+ bool sendNAK(int s, struct sockaddr * server, int serverSize);
 
- void closeSocket(int s);
+ bool getFile(std::string fileName, int s, struct sockaddr * server, socklen_t * serverSize);
 
- int sendGETRequest(int sd, sockaddr_in to, char* fileName);
+ int closeSocket(int s);
 
  int validation(struct packet);
+
+ packet byteDataToPacket(byte data[PACKETSIZE]);
 
  int reassembly(struct packet);
 
