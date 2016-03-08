098c4a5991d04bf34013d53bb6a27d545d3f7c15
Sam Bartlett <sambartlett@Sams-MacBook-Pro.local>
2016-03-07 21:10:32 -0600

    Cleaned up client file. Sends get request now.

--- client/client.cpp
+++ client/client.cpp
@@ -1,5 +1,4 @@
-
- // INCLUDE SOCKET REQUIREMENTS
+// INCLUDE SOCKET REQUIREMENTS
  #include <sys/types.h> // Basic sys data types
  #include <sys/socket.h> // Basic socked definitions
  #include <netinet/in.h> // Basic internet definitiions
@@ -14,48 +13,73 @@
  // For now we will stay with std namespace
  using namespace std;
 
- int main() {
- 	struct sockaddr_in client;
- 	struct sockaddr_in server;
- 	int sock = 0;
- 	string ip = string("131.204.14.183"); // TUX183
- 	int port = CLIENT_PORT_NUM;
+ int main(int argc, char *argv[]) {
+	if (argc != NUM_ARGS) {
+		cout << "Usage Error. Expected usage: <Program Name> <Server IP Address> <File Name>" << endl;
+		return 0;
+	}
 
- 	// Let's make a socket..
- 	if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+	int s; // store the socket id
+	string serverIP = argv[1]; // The server ip
+	string fileName = argv[2];
+	// Declare storages for our server and client addresses
+	struct sockaddr_in server;
+	struct sockaddr_in client; 
+
+	if((s = initSocket()) < 0) {
  		cout << "Error creating socket. Quiting." << endl;
- 		return 1;
- 	} else {
- 		cout << "Socket " << sock << " created!" << endl;
- 	}
+		return 0;
+	}
 
- 	// Set up the client.
- 	memset((char *)&client, 0, sizeof(client)); // removable? 
- 	client.sin_family = AF_INET;
- 	client.sin_addr.s_addr = htonl(INADDR_ANY);
- 	client.sin_port = htons(port);
+	// Set up the client.
+ 	memset((char *)&client, 0, sizeof(client)); // Delegate space in memory
+ 	client.sin_family = AF_INET; // Internet family
+ 	client.sin_addr.s_addr = htonl(INADDR_ANY); // Doesn't matter what the IP is
+ 	client.sin_port = htons(CLIENT_PORT); // Set the port to the client port
+
+ 	// Set up the server.
+ 	memset((char *)&server, 0, sizeof(server)); // Delegate space in memory
+ 	server.sin_family = AF_INET; // Internet family;
+ 	server.sin_port = htons(SERVER_PORT); // Port is the server port in header file
+ 	inet_pton(AF_INET, serverIP.c_str(), &(server.sin_addr)); // Set the IP to the IP given
 
  	// And now let's bind our client to our socket
- 	if(bind(sock, (struct sockaddr *)&client, sizeof(client)) < 0){
+ 	if(bind(s, (struct sockaddr *)&client, sizeof(client)) < 0){
  		cout << "Error binding socket to client. Quiting." << endl;
  		return 1;
  	} else {
  		cout << "Socket successfully bound to client." << endl;
  	}
 
- 	// Set up the server.
- 	memset((char *)&server, 0, sizeof(server));
- 	server.sin_family = AF_INET;
- 	server.sin_port = htons(10073);
- 	inet_pton(AF_INET, ip.c_str(), &(server.sin_addr));
+ 	// Send a get request
+ 	string message = "GET " + fileName;
+	if(!sendGet(s, message, (struct sockaddr *)&server, sizeof(server))) {
+		cout << "Sending GET request failed. Quiting." << endl;
+	}
 
- 	cout << "Bound IP address " << inet_ntoa(server.sin_addr) << " to the server" << endl << endl;
- 	int i = 1;
- 	for(;;) {
-		 	cout << "Sending Message " << i << " to " << inet_ntoa(server.sin_addr) << endl;
-		 	sendto(sock, "MESSAGE GOES HERE", 50, 0, (struct sockaddr *)&server, sizeof(server)); // 0 is flags
-		 	sleep(2);
-		 	i++;
-		}
+	// Get the file
 
- }
+	return closeSocket(s);
+}
+
+int initSocket() {
+	int sock;
+	if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+ 		return -1;
+ 	} else {
+ 		cout << "Socket " << sock << " created!" << endl;
+ 		return sock;
+ 	}
+}
+
+// Function to send get request
+bool sendGet(int s, string message, struct sockaddr * server, int serverSize) {
+	return (sendto(s, message.c_str(), BUFFSIZE, 0, server, serverSize)) >= 0; // 0 is flags
+}
+
+// Function to close the socket
+int closeSocket(int s) {
+	close(s); // Don't forget to close the other end!
+	return 1;
+}
+
--- client/client.h
+++ client/client.h
@@ -4,11 +4,15 @@
 
  #ifndef CLIENT_H
  #define CLIENT_H
+
+ #include <string>
+
  typedef unsigned char byte;
 
- const int NUM_ARGS = 3; // 2 args - argv[0] is name, argv[1] is the target server, argv[2] is file name
- const int CLIENT_PORT_NUM = 10070; // port number assigned to group
- const int SERVER_PORT_NUM = 10073; // port number assigned to group
+ #define NUM_ARGS 3 // 2 args - argv[0] is name, argv[1] is the target server, argv[2] is the desired file name
+ #define CLIENT_PORT 10070 // port number assigned to group
+ #define SERVER_PORT 10073 // port number assigned to group
+ #define BUFFSIZE 512 // Set the buffer size. Packet is 256 and must be able to hold the header, so 512 will be safe for sure.
 
  // Header of packet - contains sequence # (0 or 1) and checksum (sum of bytes)
  struct header {
@@ -23,9 +27,9 @@
 
  int initSocket();
 
- sockaddr_in getServerAddr(char* serverName);
+ bool sendGet(int s, std::string message, struct sockaddr * server, int serverSize);
 
- void closeSocket(int sd);
+ void closeSocket(int s);
 
  int sendGETRequest(int sd, sockaddr_in to, char* fileName);
 
